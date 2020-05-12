5970c971e08172de0979e4d75e8c67f2e49eff9b
Ryan <r.p.marchildon@gmail.com>
2020-05-12 15:11:05 -0400

    Adjust ResultItem layout and css

--- src/components/Result/ResultItem/ResultItem.js
+++ src/components/Result/ResultItem/ResultItem.js
@@ -1,11 +1,19 @@
 import React from "react";
 
+import classes from "./ResultItem.module.css";
+
 const ResultItem = (props) => {
   return (
-    <div>
-      <img src="/dog_image_placeholder.png" alt="dog result" />
-      <div>
-        <p>{props.breed}</p>
+    <div className={classes.ResultItem}>
+      <img
+        className={classes.image}
+        src="/dog_image_placeholder.png"
+        alt="dog result"
+      />
+      <div className={classes.details}>
+        <p>
+          <strong>{props.breed}</strong>
+        </p>
         <p>{(parseFloat(props.probability) * 100).toFixed(2) + "%"}</p>
       </div>
     </div>
